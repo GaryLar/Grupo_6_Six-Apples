7972553dee5e700e1e74806c63ae2f7727e5ab10
Karen Calisaya <karenmcalisaya@gmail.com>
2022-03-15 16:43:20 -0300

    burgerjs

---
+++ public/javascript/burger.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('#boton-menu');
+let menu = document.querySelector('#menu');
+
+button.addEventListener('click', function () {
+ menu.classList.toggle('active'); 
+ 
+ })
