5625306248cc2b3a30a38df3bf4aa3f01f8cf055
Karen Calisaya <karenmcalisaya@gmail.com>
2022-04-04 16:32:13 -0300

    changes-add files and folder

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const res = require('express/lib/response');
 const app = express();
 const path = require('path');
-
 /* requerimos dotenv para la variable de entorno */
 require('dotenv').config();
 const port = process.env.port || 3000;
+
+
+
+/* Enrutadores */
+
+
 
 /* config de archivos estaticos */
 app.use(express.static('public'));
@@ -14,13 +18,19 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.set('views', 'src/views')
 
+/* Middlewares de Rutas */
+ 
+
+
+
 /* servidor escuchando */
 app.listen(port, () => console.log(`servidor levantado en el puerto ${port} http://localhost:${port}`))
 
 
+
 app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, './views/home.html'))
-)
+) 
 
 app.get('/login', (req, res) => 
     res.sendFile(path.join(__dirname, './views/login.html'))
@@ -51,7 +61,7 @@ app.get('/ofertas', (req, res) =>
 )
 
 app.get('/admin', (req, res) => 
-    res.sendFile(path.join(__dirname, './views/admin/products/addProduct.html'))
+    res.sendFile(path.join(__dirname, './views/admin/products/editProduct.html'))
 )
 
 app.get('/profile', (req, res) => 
