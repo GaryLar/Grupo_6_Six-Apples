6a3191bbe3574f8f472e55ab565a09a9dbe817f4
Karen Calisaya <karenmcalisaya@gmail.com>
2022-04-08 16:53:06 -0300

    admin

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -2,9 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin/adminController')
+const adminProductsController = require('../controllers/admin/adminProductsController')
 
 /* GET mostrará index */
 router.get('/', adminController.index) /* pagina de inicio */
+
+router.get('/productos/agregar', adminProductsController.productAdd)
 
 /* router.get('/productos', adminProductsController.list);
 router.get('/productos/agregar', adminProductsController.productAdd); */
