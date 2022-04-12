a7c79f4da55068c2c2edea1fc75b0c4a848cff8a
Karen Calisaya <karenmcalisaya@gmail.com>
2022-04-12 17:56:29 -0300

    listProduct

--- src/controllers/admin/adminProductsController.js
+++ src/controllers/admin/adminProductsController.js
@@ -1,8 +1,16 @@
 /* controlador para los products */
+const { getProducts, writeProducts} = require('../../data/index');
+
 module.exports = {
+    list: (req, res) => {
+        res.render('admin/productsAdmin/listProduct', {
+            title: "Listado de Productos",
+            productos: getProducts
+        })
+    },
     productAdd: (req, res) => {
         res.render('admin/productsAdmin/addProduct', {
             title: "Agregar Producto"
         })
-    }
+    },
 }
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,12 +1,16 @@
 /* roteador para los CRUD DE PRODUCTOS */
+const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/admin/adminController')
-const adminProductsController = require('../controllers/admin/adminProductsController')
+const adminProductsController = require('../controllers/admin/adminProductsController');
+const { route } = require('./productsRouter');
 
 /* GET mostrará index */
 router.get('/', adminController.index) /* pagina de inicio */
 
+/* CRUD PRODUCTOS */
+router.get('/productos', adminProductsController.list);
 router.get('/productos/agregar', adminProductsController.productAdd)
 
 /* router.get('/productos', adminProductsController.list);
