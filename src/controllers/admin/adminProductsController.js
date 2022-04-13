78b960460df68521a76c15498cefa992c4498492
Gary <gary.200114@gmail.com>
2022-04-13 18:58:02 -0300

    Crud Productos

--- src/controllers/admin/adminProductsController.js
+++ src/controllers/admin/adminProductsController.js
@@ -13,4 +13,39 @@ module.exports = {
             title: "Agregar Producto"
         })
     },
+    productCreate:(req,res)=>{
+         /* 1 - Crear el objeto producto */
+         let lastId = 0;
+         getProducts.forEach(product => {
+             if(product.id > lastId){
+                 lastId = product.id;
+             }
+    })
+    let newProduct = {
+        ...req.body,      /* ... (express operator) trae todas las propiedades de  un objeto , en este caso agrega todas las propiedades a esta variable*/
+        id: lastId + 1,
+       /*  stock: req.body.stock ? true : false */
+    }
+
+    // Paso 2 - Guardar el nuevo producto en el array de productos
+    getProducts.push(newProduct)
+
+    // Paso 3 - Escribir el JSON de productos con el array actual
+    writeProducts(getProducts)
+
+    // Paso 4 - Devolver respuesta (redirección)
+    res.redirect('/admin/productos')
+},
+/* Envia la vista de form de edición de producto */
+productEdit: (req,res)=>{
+    /* 1 - Obtener el id del producto */
+    let idProducto = +req.params.id;
+     /* 2 - Buscar el producto a editar */
+     let producto = getProducts.find(producto => producto.id === idProducto)
+     /* 3 - Mostrar el producto en la vista */
+     res.render('admin/productsAdmin/editProduct', {
+         titulo: "Editar:",
+         producto
+     })
+}
 }
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -9,10 +9,25 @@ const { route } = require('./productsRouter');
 /* GET mostrará index */
 router.get('/', adminController.index) /* pagina de inicio */
 
-/* CRUD PRODUCTOS */
-router.get('/productos', adminProductsController.list);
-router.get('/productos/agregar', adminProductsController.productAdd)
 
+/* ============== */
+/* CRUD PRODUCTOS */
+/* ============== */
+/* GET - Lista de productos */
+router.get('/productos', adminProductsController.list);
+/* GET - Agregar producto */
+router.get('/productos/agregar', adminProductsController.productAdd);
+/* POST - Crea un producto en la DB */
+router.post('/productos/',adminProductsController.productCreate);
+
+
+/* GET - Editar producto */
+router.get('/productos/editar/:id', adminProductsController.productEdit)
+/* PUT - Actualiza producto en la DB */
+/* router.put('/productos/:id', adminProductsController.productUpdate); */
+/* DELETE - Elimina un producto */
+/* router.delete('/productos/eliminar/:id', adminProductsController.productDelete);
+ */
 /* router.get('/productos', adminProductsController.list);
 router.get('/productos/agregar', adminProductsController.productAdd); */
 
