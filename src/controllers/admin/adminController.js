ff53168c6389bd23934106da59fee65981f29941
Karen Calisaya <karenmcalisaya@gmail.com>
2022-04-14 16:58:50 -0300

    index admin

--- src/controllers/admin/adminController.js
+++ src/controllers/admin/adminController.js
@@ -3,7 +3,7 @@ const {getProducts} = require ('../../data');
 module.exports = {
     index: (req, res) => {
         res.render('admin/adminIndex', {
-            titulo: 'admin'
+            title: 'admin'
         })
     }
 }
