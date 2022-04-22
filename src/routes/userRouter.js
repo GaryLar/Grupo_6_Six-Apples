21bf1d9aec07eec3512eed02790ff887d83e9346
andrechacon <achacon190@gmail.com>
2022-04-22 19:02:47 -0300

    boton imagen

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,12 +2,15 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
 const productController = require("../controllers/userController");
+const uploadFile = require ('../middlewares/uploadAvatar'); 
+
 
 /* Ruta para mostrar los productos */
 router.get('/login', userController.login);
 router.get('/perfil', userController.profile);
 router.get('/registro', userController.register);
 /* post-crea un nuevo usuario */
-router.post('/registro', userController.processRegister);
+router.post('/registro',uploadFile.single('image'), userController.processRegister);
+/* Color Put(actualizar) */
 
 module.exports = router;
