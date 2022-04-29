b17212b32bf700500f2f9ca76ccdcce7805352bf
JoakoG81 <joaquingalvan81.jg@gmail.com>
2022-04-29 18:58:01 -0300

    arreglo crud

--- src/controllers/admin/adminCategoryController.js
+++ src/controllers/admin/adminCategoryController.js
@@ -15,7 +15,6 @@ module.exports = {
         })
     }, 
     categoryCreate: (req, res) => {
-        /* 1-Crear el Objeto categoria */ 
         let lastId = 0; 
         getCategories.forEach(categoria => {
             if(categoria.id > lastId){
@@ -26,29 +25,21 @@ module.exports = {
             ...req.body,
             id : lastId + 1, 
         }
-        // Paso 2 - guardar la nueva categoria en el array de categorias
         getCategories.push(newCategoria)
 
-        // Paso 3 - Escribir JSON de categories con el array actual 
         writeCategories(getCategories)
 
-        // Paso 4 - Devolver respuesta (redireccion)
         res.redirect('/admin/categorias')
     }, 
 
-    //Envia la vista de form de edicion de categoria 
     categoryEdit: (req,res) => {
-        // 1- Obtener el id de la categoria
         let idCategoria = +req.params.id; 
-        // 2- Buscar la categoria a editar 
         let categoria = getCategories.find( categoria => categoria.id === idCategoria)
-        // 3- Mostrar el producto en la vista 
         res.render('admin/categoriesAdmin/editCategory',{
             title: "Editar Categoria:", 
             categoria 
         })
     }, 
-    // recibe los datos actualizados del form de edicion 
     categoryUpdate: (req, res) => {
         let categoryId = +req.params.id; 
         getCategories.forEach(categoria => {
@@ -57,9 +48,7 @@ module.exports = {
             }
         })
 
-        // Guardarlos en la DB 
         writeCategories(getCategories); 
-        // Redireccion 
         res.redirect('/admin/categorias'); 
     }, 
 
@@ -67,14 +56,11 @@ module.exports = {
         let categoryId = +req.params.id; 
         getCategories.forEach(categorias => {
             if( categorias.id === categoryId){
-                //obtener la ubicacion (indice) de la categoria a eliminar 
                 let indiceDeCategoria = getCategories.indexOf(categorias); 
                 getCategories.splice(indiceDeCategoria, 1) 
             }
         })
-         // 3- Sobreescribir el json 
          writeCategories(getCategories); 
-         //Redireccionar 
          res.redirect('/admin/categorias'); 
     }
 
--- src/controllers/admin/adminProductsController.js
+++ src/controllers/admin/adminProductsController.js
@@ -13,8 +13,7 @@ module.exports = {
             title: "Agregar Producto"
         })
     },
-    productCreate:(req,res)=>{
-         /* 1 - Crear el objeto producto */
+    productCreate:(req,res)=>{   
          let lastId = 0;
          getProducts.forEach(product => {
              if(product.id > lastId){
@@ -22,35 +21,25 @@ module.exports = {
              }
     })
     let newProduct = {
-        ...req.body,      /* ... (express operator) trae todas las propiedades de  un objeto , en este caso agrega todas las propiedades a esta variable*/
+        ...req.body, 
         id: lastId + 1,
         image:req.file ? req.file.filename : "default-image.png",
         view: req.body.view ? true : false 
     }
-
-    // Paso 2 - Guardar el nuevo producto en el array de productos
     getProducts.push(newProduct)
-
-    // Paso 3 - Escribir el JSON de productos con el array actual
     writeProducts(getProducts)
-
-    // Paso 4 - Devolver respuesta (redirección)
     res.redirect('/admin/productos')
 },
-/* Envia la vista de form de edición de producto */
+
 productEdit: (req,res)=>{
-    /* 1 - Obtener el id del producto */
-    let idProducto = +req.params.id;
-     /* 2 - Buscar el producto a editar */
-     let producto = getProducts.find(producto => producto.id === idProducto)
-     /* 3 - Mostrar el producto en la vista */
-     res.render('admin/productsAdmin/editProduct', {
-         title: "Editar:",
-         producto
-     })
+    let idProducto = +req.params.id; 
+    let producto = getProducts.find(producto => producto.id === idProducto)
+    res.render('admin/productsAdmin/editProduct', {
+        title: "Editar:",
+        producto
+    })
 },
 
-    /* Recibe los datos actualizados del form de edición */
 productUpdate: (req, res) => {
     let productoId = +req.params.id;
     getProducts.forEach(producto => {
@@ -66,25 +55,20 @@ productUpdate: (req, res) => {
         }
     })
 
-    /* Necesitamos guardarlos en la DB */
     writeProducts(getProducts);
-    /* redireccion */
     res.redirect('/admin/productos'); 
 },
+
 productDelete: (req, res) => {
     let productoId = +req.params.id;
     getProducts.forEach(producto => {
-     if(producto.id === productoId){
-          //Obtener la ubicación (índice) del producto a eliminar
-          let indiceDeProducto = getProducts.indexOf(producto);
-          getProducts.splice(indiceDeProducto, 1)
-     }
+    if(producto.id === productoId){
+        let indiceDeProducto = getProducts.indexOf(producto);
+        getProducts.splice(indiceDeProducto, 1)
+    }
     })
-    /* 3 - Sobreescribir el json */
     writeProducts(getProducts);
-    /* REDIRECCIONAR */ 
     res.redirect('/admin/productos');
-
 }
 
 }
