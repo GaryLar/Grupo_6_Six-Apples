4c23431cb40ef8457b3686168bfde9176fe0ab03
JoakoG81 <joaquingalvan81.jg@gmail.com>
2022-05-30 18:59:51 -0300

    Model de Users

---
+++ src/database/models/User.js
@@ -0,0 +1,65 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'User';
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER(10).UNSIGNED,
+            autoIncrement: true,
+            allowNull: false, /* permite nulos? */
+            primaryKey: true,
+        },
+        name: {
+            type: dataTypes.STRING(45),
+            allowNull: false,
+        },
+        email: {
+            type: dataTypes.STRING(45),
+            allowNull: false,
+        },
+        password: {
+            type: dataTypes.STRING(70),
+            allowNull: false,
+        },
+        image: {
+            type: dataTypes.STRING(45),
+        },
+        number: {
+            type: dataTypes.INTEGER(11),
+        },
+        dni: {
+            type: dataTypes.INTEGER(11),
+        },
+        phone: {
+            type: dataTypes.INTEGER(11),
+        },
+        postCode: {
+            type: dataTypes.INTEGER(11),
+        },
+        province: {
+            type: dataTypes.STRING(100),
+        },
+        district: {
+            type: dataTypes.STRING(100),
+        },
+        direction: {
+            type: dataTypes.STRING(100),
+        },
+        rolld: {
+            type: dataTypes.INTEGER(10).UNSIGNED
+        },
+        createdAt: {
+            type: dataTypes.TIMESTAMP,
+        },
+        updatedAt: {
+            type: dataTypes.TIMESTAMP,
+        }
+    };
+    let config = {
+        tableName: 'users',
+        deletedAt: false,
+        timestamp: true
+    };
+
+    const User = sequelize.define(alias, cols, config)
+
+    return User
+}
