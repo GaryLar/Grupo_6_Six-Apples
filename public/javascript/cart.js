f55d50189799bd249202e15bd63d3640e06a0b08
JoakoG81 <joaquingalvan81.jg@gmail.com>
2022-08-03 17:51:24 -0300

    cart.js completito

--- public/javascript/cart.js
+++ public/javascript/cart.js
@@ -1,5 +1,7 @@
 const BASE_URL = window.location.origin
     let $addToCart = document.querySelector('#addToCart');
+    let $removeOne = document.querySelector('#removeOne');
+    let $removeAll = document.querySelector('#removeAll');
 
     function addToCart (productId, quantity = 1, user){
         fetch(`${BASE_URL}/api/productos/carrito/${productId}/${quantity}/${user}`, {method: "POST"})
@@ -20,5 +22,68 @@ const BASE_URL = window.location.origin
         })
         .catch(error => alert(`${error.errorMsg}`))
         
+    }
+
+    function removeOne (productId, user){
+        fetch(`${BASE_URL}/api/productos/carrito/removeOne/${productId}/${user}`, {method: "DELETE"})
+        .then(res => {
+            if(res.ok){
+                return res.json()
+            }else{
+                throw {
+                    errorMsg: "Ocurrió un error"
+                }
+            }
+        })
+        .then(result => {
+            console.log(result)
+            if(result.status === 200){
+                alert('Producto Eliminado')
+                window.location.reload()
+            }
+        })
+        .catch(error => alert(`${error.errorMsg}`))
+    }
+
+    function removeAll (productId, user){
+        fetch(`${BASE_URL}/api/productos/carrito/removeAll/${productId}/${user}`, {method: "DELETE"})
+        .then(res => {
+            if(res.ok){
+                return res.json()
+            }else{
+                throw {
+                    errorMsg: "Ocurrió un error"
+                }
+            }
+        })
+        .then(result => {
+            
+            if(result.status === 200){
+                alert('Producto Eliminado')
+                window.location.reload()
+            }
+        })
+        .catch(error => alert(`${error.errorMsg}`))
+    }
+
+    function clearCart (user){
+        fetch(`${BASE_URL}/api/productos/carrito/clearCart/${user}`, {method: "DELETE"})
+        .then(res => {
+            if(res.ok){
+                return res.json()
+            }else{
+                throw {
+                    errorMsg: "Ocurrió un error"
+                }
+            }
+        })
+        .then(result => {
+            
+            if(result.status === 200){
+                alert('Producto Eliminado')
+                window.location.reload()
+            }
+        })
+        .catch(error => alert(`${error.errorMsg}`))
     }
 
